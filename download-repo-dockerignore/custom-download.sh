b7573440e0614e1d566459e60378d4c7bb6b2c30
jiewpeng <jiewpeng.lim@gmail.com>
2019-10-18 18:56:24 +0800

    minor enhancements

--- download-repo-dockerignore/custom-download.sh
+++ download-repo-dockerignore/custom-download.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 TEMP_FOLDER=../__temp__
-TEMP_ZIP_FILE=../repo_cleaned.zip
+OUTPUT_ZIP_FILE=../repo_cleaned.zip
 REPO=https://github.com/jiewpeng/test-git-clean.git
 
 rm -rf $TEMP_FOLDER && \
-rm -f $TEMP_ZIP_FILE && \
+rm -f $OUTPUT_ZIP_FILE && \
 git clone $REPO $TEMP_FOLDER && \
 cd $TEMP_FOLDER && \
 # replace gitignore with stricter dockerignore
 mv .gitignore gitignore && \
 cp .dockerignore .gitignore && \
+# add custom files which should not be deleted, but should be 
+# ignored when building the Docker image
+echo '!Dockerfile' >> .gitignore
 # remove everything from git tracking
 git rm -r --cached . && \
 # re-add everything, excluding those ignored in dockerignore (now gitignore)
@@ -20,6 +23,6 @@ git clean -d -f -X && \
 # restore original gitignore
 rm .gitignore && \
 mv gitignore .gitignore && \
-zip -r $TEMP_ZIP_FILE . && \
+zip -r $OUTPUT_ZIP_FILE . && \
 # cleanup
 rm -rf $TEMP_FOLDER
